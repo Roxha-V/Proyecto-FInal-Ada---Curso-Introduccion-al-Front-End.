470898a8f882e15cfeca6564fef2233efa53068a
Rocha <rociovalverde1@gmail.com>
2023-09-07 11:41:56 -0300

    feat: :sparkles: generacion de filtro de busqueda- unico por gener a modo de prueba
    se genera  funcionalidad para filtrar personajes por la categoria genero Female, a modo de prueba , para implementarlo en en nuevas categorias

--- main.js
+++ main.js
@@ -6,13 +6,12 @@ const container = document.getElementById("cards-container"); /*se crea variable
 
 
 function getCharacters() {
-  //var dataApi = [];              /* aca se crea una variable llamada dataApi se le asigna un array vacío.
-  //   Esta variable se utiliza más adelante para almacenar los resultados de la API.*/
+
   fetch("https://rickandmortyapi.com/api/character")
     .then(res => res.json())
     .then(data => {
       renderCharacters(data.results);
-      // dataApi = data.results;
+
       // renderAllDataCharacters(data.results)
     })
   //return dataApi;
@@ -39,30 +38,45 @@ function renderCharacters(characters) {
       <div class="card" id="card${character.id}">  
          
         <div class="resume">
-                <h2>Este personaje se llama <br> ${character.name}</h2>
+                <h2>This Character Is Called <br> ${character.name}</h2>
                 <img src="${character.image}" alt="${character.name}">
-                <button class="detalles-personaje" onClick="toogleInfo('${character.id}')">Saber más de este personaje</button>
-            </div>
+                <button class="detalles-personaje" onClick="toogleInfo('${character.id}')"> I want to know more </button>
+        </div>
                          
-            <div class="details">
-      
-                <h2>Este personaje se llama <br> ${character.name}</h2>
+     <div class="details">
+          <div class ="saludo">
+                   <h2> Hello there!,
+                   <br> I'm 
+                   <span>${character.name}</span>
+                   </h2>
+          </div>
+       
                 <img src="${character.image}" alt="${character.name}">
-                <p>${character.gender}</p>
-                <p>${character.location.name}</p>
-                <p>${character.species}</p>
-                <p>${character.origin.name}</p>
-                <p>${character.status}</p>
+              
+          <div class ="dtls">
+                <p>Gender : ${character.gender}</p>
+                <p>Location : ${character.location.name}</p>
+                <p>Specie : ${character.species}</p>
+                <p>Origin : ${character.origin.name}</p>
+                <p>Life Status : ${character.status}</p>
+          </div>              
                 <button onClick="hideDetails('${character.id}')">Volver atrás</button>
-                </div>              
-        
-                </div>
+            
       </div>`
 
   })
 };
 
-/*<button class="saludo-button" onClick="mostrarSaludo('${character.name}')">¡Saludar!</button>*/
+
+/*Se realiza una funcion donde el forEach permite iterar y por ende
+mostrar por cada uno de los personajes que aparecen en el array
+del json los detalles de cada personaje, y  luego al haber invocado al DOM,
+traduciremos la data de la api al html utilizando los parametros que nos brinda
+el json. Cuando invocamos al dom (al container dentro del dom mejor dicho -
+<container.innerHTML += >) y guardamos elementos del html en variables para
+que podamos luego a esas variables definirlas y generar funciones, en este
+caso la variable es la que ya definimos arriba que es <container>*/
+
 
 
 function validarClassNameActivo(string) {
@@ -78,6 +92,7 @@ function toogleInfo(id) {
   let nodoPadre = document.getElementById(`card${id}`)
   //console.log(nodoPadre.className)
 
+
   if (validarClassNameActivo(nodoPadre.className)) {
     // solo me ejecuto cuando la card esta activa y deberia cambiar al estado contrario
     nodoPadre.className = nodoPadre.className.replace("active", "")
@@ -86,6 +101,7 @@ function toogleInfo(id) {
   else {
     // solo me ejecuto sivno esta acitbo por lo tanto la card no tiene clase activo y lo que deberia hacer es activar
     nodoPadre.className += " active"
+
   }
 
 }
@@ -100,19 +116,151 @@ function hideDetails(id) {
   }
 }
 
+/* puebafiltro botton genero femenino*/
+
+function womanFilter(gender, female) {
+  container.innerHTML = "";
+
+  fetch(`https://rickandmortyapi.com/api/character/?${gender}=${female}`)
+    .then(res => res.json())
+    .then(data => {
+      womenData(data.results);
+    });
+}
+
+
+
+const btnWoman = document.getElementById('btn-woman');
+btnWoman.addEventListener('click', () =>
+  womanFilter("gender", "female"))
+
+
+
+function womenData(females) {
+  females.forEach(female => {
+    container.innerHTML +=
+      `<div class="card" id="card${female.id}">  
+         
+  <div class="resume">
+          <h2>This Character Is Called <br> ${female.name}</h2>
+          <img src="${female.image}" alt="${female.name}">
+          <button class="detalles-personaje" onClick="toogleInfo('${female.id}')"> I want to know more </button>
+  </div>
+                   
+<div class="details">
+    <div class ="saludo">
+             <h2> Hello there!,
+             <br> I'm 
+             <span>${female.name}</span>
+             </h2>
+    </div>
+ 
+          <img src="${female.image}" alt="${female.name}">
+        
+    <div class ="dtls">
+          <p>Gender : ${female.gender}</p>
+          <p>Location : ${female.location.name}</p>
+          <p>Specie : ${female.species}</p>
+          <p>Origin : ${female.origin.name}</p>
+          <p>Life Status : ${female.status}</p>
+    </div>              
+          <button onClick="hideDetails('${female.id}')">Volver atrás</button>
+      
+</div>`
+
+  })
+} ``
 
 
 
 
 
-/*Se realiza una funcion donde el forEach permite iterar y por ende
-mostrar por cada uno de los personajes que aparecen en el array
-del json los detalles de cada personaje, y  luego al haber invocado al DOM,
-traduciremos la data de la api al html utilizando los parametros que nos brinda
-el json. Cuando invocamos al dom (al container dentro del dom mejor dicho -
-<container.innerHTML += >) y guardamos elementos del html en variables para
-que podamos luego a esas variables definirlas y generar funciones, en este
-caso la variable es la que ya definimos arriba que es <container>*/
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*prueva cualquier filtro permitido por la api*/
+
+function anyFilter(key, value) {
+  container.innerHTML = "";   /*llamado al container*/
+
+  fetch(`https://rickandmortyapi.com/api/character/?${key}=${value}`)
+    .then(res => res.json())
+    .then(data => {
+      infoCharacters(data.results);
+    });
+}
+
+/*para que sea más dinámica la func. y acepte otros parámetros
+que filtren,se cambian los parametros gender/female para que
+tome los parámetros key y value, que son los que representarán
+la clave y el valor por los cuales se quiere filtrar los datos que tare la application*/
+
+const btnFiltro = document.getElementById('btn-custom-filter');
+
+
+/*
+btnFiltro.addEventListener('click', () => {
+  const key = "gender"; // Reemplaza con la clave por la que deseas filtrar
+  const value = "female"; // Reemplaza con el valor por el que deseas filtrar
+  customFilter(key, value);
+}); */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*background-image: url(img/fondo1.jpg);
+
+    background-size: cover;
+
+    background-repeat: no-repeat;
+
+    background-position: center;
+
+    background-attachment: fixed;
+
+    si pongo imagen de tras par aque se mueva pero solo las imagenes*/
