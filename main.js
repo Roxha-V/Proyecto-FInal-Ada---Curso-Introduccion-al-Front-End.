5d119e443e2e3d7c30b304e438e4b1fac52b636c
Rocha <rociovalverde1@gmail.com>
2023-09-14 07:43:13 -0300

    feat: :sparkles: Cambio de estilo - Mejora en Funcionalidad filtrado de personajes - Incorporacion de Funcionalidad para cambiar de paginas.
    Se cambia el estilo de la cards, el fondo, la apertura de cards con gif , tambien se suma una mejor barra de navegacion.  En cuanto a funcionalidad se mejora la calidad del codigo en torno a filtrar personajes y se agrega la fucionalidad de poder cambiar de pagina (hacia adelante o hacia atras) o ir a la primera o ultima pagina de la cards con personajes sin filtrar.

--- main.js
+++ main.js
@@ -1,31 +1,44 @@
-const container = document.getElementById("cards-container"); /*se crea variable para definir que cuando se llame a esa variable, 
+const container = document.getElementById("cards-container"); /*console.log (container)*/ /*se crea variable para definir que cuando se llame a esa variable, 
                                                                es porque se esta seleccionando a los elementos dentro de main-container,
                                                               que es un elemento del DOM - siempre esto se pone por encima ya que se
                                                               llama de forma global*/
-/*console.log (container)*/
+
+var _ = "" // Variable que define que _ es igual a nada*/
+let info
+let episode
 
 
 
 
+/*funcion comprobada y chequeada solo datos character*/
+function getCharacters(genderValue = "", statusValue = "", speciesValue = "", pageNumber = 1) {
+  /* Esta es una funcion para obtener personajes con opciones de filtro, lo que hace es que cuando la funcion se llame
+  los parametros que se le pasen "filtraran" el resultado, hay que tener en cuenta que hay que respetar los lugares de los parametros,
+  y que si no por ej quiero filtrar solo por status seria 
+  const aliveCharacters = getCharacters("", "alive", "","")  lo que hace es  - console.log('getcharacter values', genderValue, statusValue, statusValue, pageNumber)*/
 
-/*funcion comprobada y chequeada _solo datos character*/
-function getCharacters(key, value) {
-  container.innerHTML = ""
+  container.innerHTML = "";
 
-
-  fetch(`https://rickandmortyapi.com/api/character?${key}=${value}`)
+  fetch(`https://rickandmortyapi.com/api/character?gender=${genderValue}&status=${statusValue}&species=${speciesValue}&page=${pageNumber}`)
     .then(res => res.json())
     .then(data => {
-      renderCharacters(data.results);
+
+      renderInfo(data.info)  /*info y data son etiquetas que ya vienen en el json de la api para buscar informacion*/
+      totalPages = (data.info.pages) /*totalpages es una variable No una func)*/
+      renderCharacters(data.results)
+      renderPages()
     })
     .catch(error => {
       console.error("Error trayendo data personaje", error);
-    });
+    })
 }
 
-
+function renderInfo(objInfo) {
+  Informacion = objInfo;
+}
 getCharacters();
-console.log(getCharacters)
+/*console.log(getCharacters) - ver si funciona la funcion*/
+
 
 
 function renderCharacters(characters) {
@@ -64,44 +77,169 @@ function renderCharacters(characters) {
   })
 }
 
+
+/*paginacion*/
+
+/*variables*/
+let actualPage = 1; /* contador - rastrea pag actual */
+
+let totalPages = 1; /* almacena el número total de páginas */
+
+const btnPrev = document.getElementById("btn-prev");
+const btnNext = document.getElementById("btn-next");
+const btnFirst = document.getElementById("btn-first");
+const btnLast = document.getElementById("btn-last");
+const pageNumbers = document.getElementById("page-numbers");
+
+
+function renderPages() {
+  pageNumbers.innerHTML += `Página ${actualPage} de ${totalPages}`;
+
+  console.log(`Página ${actualPage} de ${totalPages}`)
+}
+
+btnFirst.addEventListener('click', function () {
+  getCharacters(_, _, _, actualPage);
+  updatePrevBtn();
+  updateNextBtn();
+  renderPages();
+});
+
+btnLast.addEventListener('click', function () {
+  actualPage = totalPages
+  getCharacters(_, _, _, actualPage);
+  updatePrevBtn();
+  updateNextBtn();
+  renderPages();
+});
+
+
+
+
+/*NEXT BTN*/
+
+
+btnNext.addEventListener('click', function () {   //console.log("valor actualPage antes de hacer click en nxt btn", actualPage);
+
+  actualPage++ /* incrementa valor actualPage*/ // console.log("valor actualPage despues de hacer click en nxt btn", actualPage);
+
+  getCharacters(_, _, _, actualPage);
+
+  updatePrevBtn()
+
+  renderPages()
+});
+
+/*aqui se le añade un una funcion al evento click, que lo que produce es que
+al hacer click al boton "btnNext" el valor de actualpage, que se pasa como parametro
+para la funcion getCharacter, incremente, por ende al incrementar el valor, tambien
+incrementara la pagina en la que nos encontremos*/
+
+
+/*PREV BTN*/
+
+btnPrev.addEventListener('click', () => {
+  //console.log("valor actualPage antes de hacer click en prev btn", actualPage)
+
+  actualPage--;
+  //console.log("valor actualPage despues de hacer click en prev btn", actualPage)
+
+  updatePrevBtn();
+
+  getCharacters(_, _, _, actualPage);
+
+  renderPages()
+
+});
+
+/*aqui se le añade un una funcion al evento click, que lo que produce es que
+al hacer click al boton "btnPrev" el valor de actualpage, que se pasa como parametro
+para la funcion getCharacter, vaya en decrimento x 1, por ende al bajar el valor, tambien
+"bajara" la pagina en la que nos encontremos */
+
+
+
+
+
+/*paginacion btns prev y next disable*/
+
+function updatePrevBtn() {
+  //console.log("valor actualpage cuando se hace update btn prev", actualPage)
+
+  if (actualPage > 1) {
+    btnPrev.disabled = false;
+
+    //console.log("me ejecuto cuando actualpage > 1", actualPage)
+
+  }
+  if (actualPage <= 1) {
+    btnPrev.disabled = true;
+    /*console.log("me ejecuto cuando actualpage  < 1", actualPage)*/
+
+  }
+
+}
+
+/*la funcion updateprevbtn expresa una condicion  donde si el valor de actualpage
+es mayor a uno, el btnPrev permanece habilitado (ya que sera falso para disable),
+mientras que si el valor de actualpage es menor a 1 el btnprev se dehabilitara
+(ya que sera true para disable)*/
+
+
+function updateNextBtn() {
+  if (actualPage >= totalPages) {
+    btnNext.disabled = true; // Deshabilita el botón "Siguiente" si estás en la última página o más allá.
+  } else {
+    btnNext.disabled = false; // Habilita el botón "Siguiente" si no estás en la última página.
+  }
+}
+
+
+
+/*verifica si la página actual es = o mayor al n total de 
+páginas, por ende si la condicion se cumple,
+ se esta en la ultima pagina y btn de pag siguiente se desabilita.*/
+
+
+
 /*Portal gif*/
 
-function mostrarPortal() {
+function mostrarPortal() {  //funcion para mostrar "portal" //
   const portalLoading = document.getElementById("loading-gif");
   portalLoading.style.display = "flex";
 }
 
-function ocultarPortal() {
+function ocultarPortal() {  //funcion para ocultar "portal" //
   const portalLoading = document.getElementById("loading-gif");
+  portalLoading.style.animation = "none";
   portalLoading.style.display = "none";
 }
 
+/* activar y desactivar card- toogle*/
 
-function validarClassNameActivo(string) {
-  // parametro es de tipo string 
-  // operar para buscar una coincidencia con palabera active , osea si el string contiene 
-  // si enctra coincidencia es true 
+
+function validarClassNameActivo(string) { /*parametro es de tipo string , opera para buscar una coincidencia con palabera active , osea si el string contiene (osea si encuentra coincidencia es true) */
   return string.includes("active")
 
 }
 
-function toogleInfo(id) {
-  // esta fn cambia los estados entre activo y inactivo, osea alterna la clase "active" en un elemento DOM (card/nodopadre) con el ID que se le da.
+function toogleInfo(id) {  // esta fn cambia los estados entre activo y inactivo, osea alterna la clase "active" en un elemento DOM (card/nodopadre) con el ID que se le da.
   mostrarPortal(); /*muestra el gif al tocar el boton de info del personaje*/
-  let nodoPadre = document.getElementById(`card${id}`)
-  //console.log(nodoPadre.className)
 
-  setTimeout(() => {   /*establece un tiempo de demora que dura la funcionalidad*/
+  let nodoPadre = document.getElementById(`card${id}`) //console.log(nodoPadre.className)
+
+
+  setTimeout(() => {   /*establece un tiempo de demora que dura la funcion*/
     ocultarPortal();
-  }, 1000);
+  }, 2700);
 
-  if (validarClassNameActivo(nodoPadre.className)) {
-    // solo me ejecuto cuando la card esta activa y deberia cambiar al estado contrario
+  if (validarClassNameActivo(nodoPadre.className)) { // condicion:solo me ejecuto cuando la card esta activa y deberia cambiar al estado contrario
+
     nodoPadre.className = nodoPadre.className.replace("active", "")
 
   }
-  else {
-    // solo me ejecuto sivno esta acitbo por lo tanto la card no tiene clase activo y lo que deberia hacer es activar
+  else { // condicion: solo me ejecuto sino la card esta "active", por lo tanto la card no tiene clase activo y lo que deberia hacer es activar
+
     nodoPadre.className += " active"
 
   }
@@ -114,9 +252,10 @@ function hideDetails(id) {
 
   if (validarClassNameActivo(nodoPadre.className)) {
 
-    nodoPadre.classList.toggle("active");
+    nodoPadre.classList.toggle("active");  //toogle cambia de un estadoa otro
   }
 }
+
 
 
 
@@ -125,90 +264,74 @@ function hideDetails(id) {
 /*filtro female*/
 
 const btnFemale = document.getElementById('btn-female');
-
 btnFemale.addEventListener('click', () => {
-  let key = "gender"
-  let value = "female"
-
-  getCharacters(key, value)
+  getCharacters("female");
 });
 
 /*filtro male*/
 
 const btnMale = document.getElementById('btn-male');
-
 btnMale.addEventListener('click', () => {
-  let key = "gender"
-  let value = "male"
+  getCharacters("male");
 
-  getCharacters(key, value)
 });
 
 
 /*filtro gender/unknow*/
 
 const btnUnknownGender = document.getElementById('btn-unknown-gender');
-
 btnUnknownGender.addEventListener('click', () => {
-  let key = "gender"
-  let value = "Unknown"
-
-  getCharacters(key, value)
+  getCharacters("Unknown");
 });
 
+/*filtro gender/genderless*/
 
-/*filtro status*/
+const btnGenderLess = document.getElementById('btn-gender-less');
+btnGenderLess.addEventListener('click', () => {
+  getCharacters("Genderless");
+});
+
+/*Filtros status*/
 
 const btnStatusAlive = document.getElementById('btn-alive');
-
 btnStatusAlive.addEventListener('click', () => {
-  let key = "status"
-  let value = "Alive"
-
-  getCharacters(key, value)
+  getCharacters("", "Alive")
 });
 
 
 const btnStatusDead = document.getElementById('btn-dead');
-
 btnStatusDead.addEventListener('click', () => {
-  let key = "status"
-  let value = "Dead"
-
-  getCharacters(key, value)
+  getCharacters("", "Dead")
 });
-
 
 
 const btnStatusUnknown = document.getElementById('btn-unknown-status');
-
 btnStatusUnknown.addEventListener('click', () => {
-  let key = "status"
-  let value = "Unknown"
-
-  getCharacters(key, value)
+  getCharacters("", "Unknown")
 });
 
-/*Filtro specie*/
+
+/*Filtros specie*/
 
 const btnSpecieHuman = document.getElementById('btn-human');
-
 btnSpecieHuman.addEventListener('click', () => {
-  let key = "species"
-  let value = "human"
-
-  getCharacters(key, value)
-});
+  getCharacters("", "", "Human")
+})
 
 const btnSpecieAlien = document.getElementById('btn-alien');
 
 btnSpecieAlien.addEventListener('click', () => {
-  let key = "species"
-  let value = "alien"
-
-  getCharacters(key, value)
+  getCharacters("", "", "Alien")
 });
 
+
+
+/* button home*/
+
+const homeBtn = document.getElementById("btn-home")
+homeBtn.addEventListener('click', () => {
+  window.location.href = "index.html"
+});
 
 
 
@@ -221,18 +344,6 @@ el json. Cuando invocamos al dom (al container dentro del dom mejor dicho -
 <container.innerHTML += >) y guardamos elementos del html en variables para
 que podamos luego a esas variables definirlas y generar funciones, en este
 caso la variable es la que ya definimos arriba que es <container>*/
-
-
-
-
-
-
-/* button home*/
-
-const btnHome = document.getElementById('btn-home');
-btnHome.addEventListener('click', () =>
-  location.href = "/")
-
 
 
 
@@ -267,31 +378,31 @@ btnHome.addEventListener('click', () =>
 
 const promises = [
   `https://rickandmortyapi.com/api/character/`,
-  `https://rickandmortyapi.com/api/episode`,
+    `https://rickandmortyapi.com/api/episode`,
 ];
 
 
-function getCharacter(key, value) {
-  container.innerHTML = ""
+    function getCharacter(key, value) {
+      container.innerHTML = ""
 
-  const fetchData = async (promises) => {
-    try {
-      await Promise.all(
-        promises.map((promise) =>
-          fetch(promise)
-            .then((res) => res.json())
-            .then((data) => {
-              renderCharacters(data.results);
-            })
-        )
-      );
-    } catch (err) {
-      console.log("Error trayendo data personaje:", err);
+      const fetchData = async (promises) => {
+        try {
+          await Promise.all(
+            promises.map((promise) =>
+              fetch(promise)
+                .then((res) => res.json())
+                .then((data) => {
+                  renderCharacters(data.results);
+                })
+            )
+          );
+        } catch (err) {
+          console.log("Error trayendo data personaje:", err);
+        }
+      };
+      fetchData(promises)
     }
-  };
-  fetchData(promises)
-}
-*/
+* /
 
 /*pedido de la info hacia la api (fetch + endpoint, el primer .then (entonces) es una promesa,
 un metodo que registrara la rta del pedido hacia la api respuesta es un parametro que implica
@@ -435,4 +546,4 @@ btnMale.addEventListener('click', () => {
 
     background-attachment: fixed;
 
-    si pongo imagen de tras par aque se mueva pero solo las imagenes */
+    si pongo imagen de tras par aque se mueva pero solo las imagenes*/
